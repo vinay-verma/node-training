d096c84f5d57eda7700335881fd637d354306a09
vinay <vinay.an32@gmail.com>
2019-10-28 23:50:01 +0530

    working with express.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
-const http = require('http');
-const routes = require('./routes');
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
 
-var server = http.createServer(routes);
-server.listen(3000);
+const adminRoutes = require('./routes/adminRoutes');
+const shopRoutes = require('./routes/shopRoutes');
+
+const rootDir = require('./util/path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    return res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
+
+app.listen(3000);
